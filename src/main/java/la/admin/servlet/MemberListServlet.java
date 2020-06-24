dacb9b2c52d904b3632ff4972fef42222491bdb4
Makoto Takei <makoto.takei.tm@nttdocomo.com>
2020-06-24 10:24:27 +0900

    add memberlist for admin

--- src/main/java/la/admin/servlet/MemberListServlet.java
+++ src/main/java/la/admin/servlet/MemberListServlet.java
@@ -1,11 +1,17 @@
 package la.admin.servlet;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import la.bean.MemberBeans;
+import la.dao.MemberDao;
 
 /**
  * Servlet implementation class MemberListServlet
@@ -13,29 +19,35 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/MemberListServlet")
 public class MemberListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public MemberListServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public MemberListServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+		try {
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+			MemberDao dao = new MemberDao();
 
+			List<MemberBeans> list = dao.searchAll();
+			request.setAttribute("members", list);
+
+			RequestDispatcher rd = request.getRequestDispatcher("/Member/AdminDisplayMember.jsp");
+			rd.forward(request, response);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			request.setAttribute("message", "内部エラーが発生しました。");
+			RequestDispatcher rd = request.getRequestDispatcher("/errInternal.jsp");
+			rd.forward(request, response);
+
+		}	}
 }
