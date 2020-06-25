b6b39b9eddf8436bf8c56374ee3e89304bac1654
Makoto Takei <makoto.takei.tm@nttdocomo.com>
2020-06-25 11:20:21 +0900

    add PurchaseDisplayServlet

--- src/main/java/la/purchase/servlet/PurchaseDisplayServlet.java
+++ src/main/java/la/purchase/servlet/PurchaseDisplayServlet.java
@@ -9,22 +9,21 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import la.bean.ExhibitBeans;
 import la.dao.ExhibitDao;
 
 /**
- * Servlet implementation class PurchaseHistoryServlet
+ * Servlet implementation class PurchaseDisplayServlet
  */
-@WebServlet("/PurchaseHistoryServlet")
-public class PurchaseHistoryServlet extends HttpServlet {
+@WebServlet("/PurchaseDisplayServlet")
+public class PurchaseDisplayServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
-	public PurchaseHistoryServlet() {
+	public PurchaseDisplayServlet() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
@@ -36,25 +35,14 @@ public class PurchaseHistoryServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		try {
-			//セッションの確認
-			HttpSession session = request.getSession(false);
-
 			//出品DAOの呼び出し
 			ExhibitDao dao = new ExhibitDao();
 
-			//セッション管理でログインの有無確認
-			if (session != null && session.getAttribute("id") != null) {
-
-				//セッションから出品者IDの呼び出し
-				int buyerid = (Integer) session.getAttribute("id");
-
-				//自分が出品した本の設定
-				List<ExhibitBeans> mylist = dao.searchByBuyerId(buyerid);
-				request.setAttribute("purchase_history", mylist);
-			}
+			List<ExhibitBeans> list = dao.seachAllBuy();
+			request.setAttribute("purchase_all", list);
 
 			//出品表示
-			RequestDispatcher rd = request.getRequestDispatcher("/Purchase/PurchaseHistory.jsp");
+			RequestDispatcher rd = request.getRequestDispatcher("/Admin/AdminDisplayPurchase.jsp");
 			rd.forward(request, response);
 
 		} catch (Exception e) {
@@ -65,4 +53,5 @@ public class PurchaseHistoryServlet extends HttpServlet {
 
 		}
 	}
+
 }
